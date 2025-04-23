a2339f0188603e62d2fde23f69f0e1389be46d82
Akram Guroo <akramguroo.dev@gmail.com>
2025-04-23 09:58:06 +0530

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -75,5 +75,19 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
+export function updateDeliveryOptionId(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
+
 
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {
   cart, removeFromCart, calculateCartQuantity, 
-  updateQuantity
+  updateQuantity, updateDeliveryOptionId
 } from '../data/cart.js';
 import {products} from '../data/products.js';
 import formatCurrency from './utils/money.js';
@@ -158,7 +158,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio" ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
           name="delivery-option-${matchingProduct.id}">
@@ -176,5 +178,13 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
   
   return html;
 }
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const productId = element.dataset.productId;
+    const deliveryOptionId = element.dataset.deliveryOptionId;
+    updateDeliveryOptionId(productId, deliveryOptionId);
+  });
+});
 
 
